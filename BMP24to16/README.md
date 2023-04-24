446b0ee625f7df0de39de98e431596ad12b07e6b
PyroSamurai <pyrosamurai@member.fsf.org>
2023-04-24 16:54:30 -0400

    Add README to BMP24to16

---
+++ BMP24to16/README.md
@@ -0,0 +1,8 @@
+# BMP24to16
+Simple utilitarian program I made to allow me to strictly convert BMP
+ between 24bit and various 16bit formats without opening an image editor.
+
+__License__: GPLv3+
+
+__Author__: [Sean Stafford](http://github.com/PyroSamurai)
+
--- BMP24to16/compile-jar.sh
+++ BMP24to16/compile-jar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -f BMP24to16.jar
 cd ..
-jar cf BMP24to16/src/BMP24to16.jar BMP24to16/src/*.java BMP24to16/LICENSE
+jar cf BMP24to16/src/BMP24to16.jar BMP24to16/src/*.java BMP24to16/LICENSE BMP24to16/README.md
 cd BMP24to16/src
 javac *.java
 jar ufe BMP24to16.jar Main *.class
